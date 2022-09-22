a7c6cfa61e90ba26b82e9bb03303909bdaf2dfeb
Emmanuel Ojighoro <awesomeemoj@gmail.com>
2022-09-22 12:05:39 +0100

    added create react app and express generator

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const logger = require('morgan')
 const connectDB = require('./config/database')
+const cors = require('cors')
 
 //Use .env file in config folder
 require('dotenv').config({ path: "./config/.env"})
@@ -11,6 +12,9 @@ connectDB()
 
 //logging
 app.use(logger('dev'))
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.listen(process.env.PORT, () => {
     console.log('Server is running, betta go catch it!')
