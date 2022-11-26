9b5803c97617d58587672d1ca1d2d906cff9de34
Emmanuel Ojighoro <awesomeemoj@gmail.com>
2022-11-26 18:21:27 +0100

    bug fixes

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ connectDB()
 app.use(logger('dev'))
 
 app.use(cors())
-app.use(express.static('public', path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")))
 app.set("views", path.join(__dirname, "views"))
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: true}))
